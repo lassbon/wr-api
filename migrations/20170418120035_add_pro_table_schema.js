f407867f32bbe7543527be50f5e724d1223a711c
Lawrence Agbani <lawrence.agbani@Konga-Raznerd.local>
2018-05-08 06:51:18 +0100

    Modified migration scripts, seperating concerns properly with regards to user types

--- migrations/20170418120035_add_pro_table_schema.js
+++ migrations/20170418120035_add_pro_table_schema.js
@@ -2,29 +2,9 @@
 
 exports.up = function (knex) {
     return knex.schema
-        .createTableIfNotExists('pro', function (table) {
+        .createTableIfNotExists('user_type', function (table) {
             table.bigincrements('id').primary();
-            table.string('proID');
-            table.string('proFirstname');
-            table.string('proLastname');
-            table.string('proOthername');
-            table.string('proPhoneNumber');
-            table.string('proAddress');
-            table.string('proEmail').notNullable().unique();
-            table.string('proDOB');
-            table.string('country');
-            table.string('state');
-            table.string('proImageURL');
-            table.integer('proAccountNumber');
-            table.string('proAccountName');
-            table.integer('proBVN');
-            table.boolean('termsAndCondition');
-            table.boolean('isApproved');
-            table.boolean('isFacebook');
-            table.string('proStatus');
-            table.string('referralCode');
-            table.string('invitedByReferralCode');
-            table.string('password');
+            table.string('name');
             table.timestamps(true, true);
         });
 };
